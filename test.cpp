205587cb349a9c36b2be36510d746258701435d3
yxh <18903651472@163.com>
2020-11-19 23:45:16 +0800

    【描述】重写了test.cpp文件，并调用了xxx的NewInterfaceToCall接口

--- test.cpp
+++ test.cpp
@@ -5,23 +5,11 @@
 #include "context.h"
 #include <iostream>
 
-using namespace std;
-
-void callNewInterface()
-{
-	return;
-}
 
 int main(int argc, char *argv[]){
-	Strategy *ps = new ConcreteStrategyA();
-	Context *pc = new Context(ps);	
-	pc->doAction();
-	if(NULL != pc){
-		delete pc;
-	}
 	
-	//call new interface
-	callNewInterface();
+	//...
+	NewInterfaceToCall();
 	
-	return 0;		
+	return 0;
 }
